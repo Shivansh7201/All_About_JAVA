c2e13aa55b8f8242e9a7450bcd94cd9909623316
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2023-10-09 00:16:47 +0530

    Add files via upload

---
+++ Characters.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Characters{
+public static void main(String[]args){
+
+char ch1,ch2;
+
+ch1=88; //code for X
+ch2='y';
+
+System.out.println("ch1 and ch2 :");
+System.out.println(ch1+" "+ch2);
+}
+}
+
