a311d77d7a2de27431093f43ab1e718835c98135
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2023-10-09 00:16:25 +0530

    Add files via upload

---
+++ Character2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// char variables behave like INTEGERS 
+
+public class Character2{
+
+public static void main(String[]args){
+
+char ch1;
+
+ch1='X';
+
+System.out.println("ch1 contains "+ ch1);
+
+ch1++; // increment ch1
+
+System.out.println("ch1 is now "+ ch1);
+
+}
+}
