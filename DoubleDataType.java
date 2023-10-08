3df7c20cb2f7ad7c51ef8e48646154fc5d7f4131
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2023-10-09 00:18:02 +0530

    Add files via upload

---
+++ DoubleDataType.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class DoubleDataType{
+
+public static void main(String[]args){
+
+double pi,r,a;
+
+r=10.8; //radius of circle
+pi=3.14; //pi.approximately
+a=pi*r*r; // compute area
+
+System.out.println("Area of circle is " + a);
+
+}
+}
