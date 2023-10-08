0b2fffbda69e9fc3fb265c3a3dab0dd08b96045d
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2023-10-09 00:20:56 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Variables{
+
+public static void main(String[]args){
+
+int a,b,c; // declares three integers values a,b,c 
+int d=3,e=3,f=5; // declares three integers as well as initialize paralelly
+
+byte z=23; // initialize (z)
+
+double pi =3.14;
+
+char s='S';
+
+System.out.println(s);
+System.out.println();
+System.out.println("The Approx. value of PI:" + pi +"and z's :-" + z);
+System.out.println();
+System.out.println(d*e*f);
+}
+}
