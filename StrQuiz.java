1d6e9a0321e7f8ca0fd7f56d327670f22146b43e
shivansh gupta <105603160+Shivansh7201@users.noreply.github.com>
2023-11-07 23:52:51 +0530

    Add files via upload

---
+++ StrQuiz.java
@@ -0,0 +1,9 @@
+public class StrQuiz {public static void main(String[] args) {
+    String obj="Hello";
+    String obj1="java";
+    String obj2="Hello";
+    System.out.println(obj.equals(obj1) + " "+obj.equals(obj2));
+    
+}
+    
+}
